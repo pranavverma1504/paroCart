7649950d151f182e723fe163426762f6373c2609
pranav verma <130182705+pranavverma1504@users.noreply.github.com>
2025-10-26 16:38:57 +0530

    Update CORS origin for production environment

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ let app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
- origin:["http://localhost:5173" , "http://localhost:5174"],
+ origin:["https://parocart-frontend.onrender.com" , "https://parocart-admin.onrender.com"],
  credentials:true
 }))
 
