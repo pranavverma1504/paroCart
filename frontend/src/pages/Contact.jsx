533f6a3a194bc87685aafa5b636bd59ad26c7c52
pranav verma <130182705+pranavverma1504@users.noreply.github.com>
2025-10-26 17:51:32 +0530

    Update

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -21,7 +21,7 @@ function Contact() {
           <p>tel: +91-9999922222</p>
           <p>Email: admin@parocart.com</p>
         </p>
-        <p className='lg:w-[80%] w-[100%] text-[15px] text-[white] lg:text-[18px] mt-[10px] font-bold'>Careers at OneCart</p>
+        <p className='lg:w-[80%] w-[100%] text-[15px] text-[white] lg:text-[18px] mt-[10px] font-bold'>Careers at ParoCart</p>
         <p className='lg:w-[80%] w-[100%] text-[white] md:text-[16px] text-[13px]'>Learn more about our teams and job openings</p>
         <button className='px-[30px] py-[20px] flex items-center justify-center text-[white] bg-transparent border active:bg-slate-600 rounded-md' >Explore Jobs</button>
         </div>
